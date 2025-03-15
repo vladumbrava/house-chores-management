2f11536f4b36dab63f45ac012f0c0476d5aeb1a8
vladumbrava <vladdumbrava5@gmail.com>
2025-03-15 20:29:21 +0200

    Implement DBChoreRepo

--- src/repository/DBChoreRepository.java
+++ src/repository/DBChoreRepository.java
@@ -35,5 +35,41 @@ public class DBChoreRepository extends MemoryRepository<UUID, Chore>{
         }
     }
 
+    @Override
+    public void add(UUID id, Chore object) {
+        super.add(id, object);
 
+        try (Connection connection = DriverManager.getConnection(URL);
+        PreparedStatement statement =
+                connection.prepareStatement("INSERT INTO Chore VALUES (?,?,?,?,?,?,?);")) {
+            statement.setString(1,object.getID().toString());
+            statement.setString(2,object.getChoreAssignedMemberID().toString());
+            statement.setString(3,object.getChoreTitle());
+            statement.setString(4,object.getChoreDescription());
+            statement.setString(5,object.getChoreDeadline().toString());
+            statement.setInt(6,object.getChoreAssignedPoints());
+            statement.setString(7,object.getChoreStatus().toString());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void delete(UUID id) {
+        super.delete(id);
+
+        try (Connection connection = DriverManager.getConnection(URL);
+             PreparedStatement statement = connection.prepareStatement("DELETE FROM Chore WHERE choreID = ?")) {
+            statement.setString(1, id.toString());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Chore findByID(UUID id) {
+        return super.findByID(id);
+    }
 }
